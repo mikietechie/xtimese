c70699cec56af800a1e742f0a05c639cb466292b
mikietechie <mzinyoni7@outlook.com>
2023-05-10 22:07:59 +0200

    Setup a multi tenant demo app using single db, single schema, collection level architecture

---
+++ src/management/createsuperuser.js
@@ -0,0 +1,28 @@
+const initDB = require('../utils/init-db')
+const User = require('../models/user')
+const readline = require('readline')
+const util = require('util')
+
+const main = async () => {
+    await initDB()
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    const question = util.promisify(rl.question).bind(rl)
+    const firstName = await question("First name: ")
+    const lastName = await question("Last name: ")
+    const username = await question("Username: ")
+    const password = await question("Password: ")
+    const role = "superuser"
+    const user = await User.create({
+        username,
+        password,
+        firstName,
+        lastName,
+        role
+    })
+    console.log("User created")
+}
+
+main().catch(console.error).finally(() => process.exit(0))
